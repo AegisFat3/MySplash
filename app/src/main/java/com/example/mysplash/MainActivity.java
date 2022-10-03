31d481be2b26f5ff49f0441303887cfec33e5ebb
AegisFat3 <brian_cabo17@hotmail.com>
2022-10-02 23:50:28 -0500

    Primera Version

---
+++ app/src/main/java/com/example/mysplash/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mysplash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this , login_activity.class);
+                startActivity( intent );
+                finish();
+
+            }
+        }, 4000);
+    }
+}
